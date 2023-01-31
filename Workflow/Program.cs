68296fd08350b78143f5ff1f18d611f5e5f4c5a5
jamesd <dawsonjames027@gmail.com>
2023-01-31 22:59:01 +0000

    completed alphnumber

--- Workflow/Program.cs
+++ Workflow/Program.cs
@@ -1,11 +1,1 @@
-﻿using Workflow;
-
-    AlphNumber a = new AlphNumber("a2vc");
-    Console.WriteLine(a.AlphValue);
-    Console.WriteLine(a.DecimalValue);
-    a.Increment();
-    Console.WriteLine(a.AlphValue);
-    Console.WriteLine(a.DecimalValue);
-    a.Decrement();
-    Console.WriteLine(a.AlphValue);
-    Console.WriteLine(a.DecimalValue);
+﻿
